9fee21105e42ac267c57bb76acd5b24524b7d991
Ripal Patel <ripal212007@gmail.com>
2019-03-10 15:30:12 -0500

    Working

--- JosephusProblem/Program.cs
+++ JosephusProblem/Program.cs
@@ -6,7 +6,25 @@ namespace JosephusProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter number of people.");
+            var numberOfPeople = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Please enter killing interval number. It must be greater than 0.");
+            var killingInterval = Convert.ToInt32(Console.ReadLine());
+            var position = LastPersonStanding(numberOfPeople, killingInterval);
+            Console.WriteLine($"The last person standing in ({numberOfPeople}, {killingInterval}) is at {position} position");
+            Console.ReadLine();
+        }
+        public static int LastPersonStanding(int numberOfPeople, int killingInterval)
+        {
+            if (numberOfPeople == 1)
+            {
+                return 1;
+            }
+            else {
+                // The position of LastPersonStanding is adjusted due to recursive call to (numberOfPeople - 1, killingInterval) 
+                // and it considers the original position killingInterval%numberOfpeople +1 as 1
+                return (LastPersonStanding(numberOfPeople - 1, killingInterval) + killingInterval - 1) % numberOfPeople + 1;
+            }
         }
     }
 }
